2f35e7003db1e826c3d87dc687331f57efbaf85f
Alisha Soedamah <alisha.soedamah@gmail.com>
2026-06-22 09:32:01 +0200

    init leerdoelen

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alishas blog';
-export const SITE_DESCRIPTION = 'Welcome';
+export const SITE_TITLE = 'WN blog - Alisha';
+export const SITE_DESCRIPTION = 'This is my WN blog site!';
