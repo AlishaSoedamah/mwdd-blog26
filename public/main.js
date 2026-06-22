e6cedce11bd96c3bcea272e85ebc65e1bdea4aa5
Alisha Soedamah <alisha.soedamah@gmail.com>
2026-06-22 17:26:08 +0200

    inleveren

---
+++ public/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('click', (event) => {
+	document.querySelectorAll('details.auto-close').forEach((details) => {
+		if (details.open && !details.contains(event.target)) {
+			details.open = false;
+		}
+	});
+});
